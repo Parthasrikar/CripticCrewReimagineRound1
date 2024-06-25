d286f0cd7cdf49d4b644d78e451b99e62f1b6a28
Partha <parthasrikar853@gmail.com>
2024-06-25 07:34:56 +0530

    added all existing files

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+Shery.makeMagnet(".magnet-target" /* Element to target.*/, {
+    //Parameters are optional.
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1.5,
+  });
+
+  Shery.mouseFollower({
+    //Parameters are optional.
+    skew: false,
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 0.5,
+  });
+
